7e26cea46690c7e393110bc374494f494a8efc00
Caleb Parikh <parikhcaleb@gmail.com>
2025-12-21 11:03:24 +0000

    Configure base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://calebp98.github.io',
+  base: '/wedding-site',
+});
